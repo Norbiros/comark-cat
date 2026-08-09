32944e7176c51b792d4e6b433c0170332261d608
norbiros <norbiros@protonmail.com>
2026-08-09 13:10:58 +0200

    chore: remove library exports from CLI package

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@
 import { runMain } from "citty";
 import { main } from "./cli.ts";
 
-export { main, parseWidth } from "./cli.ts";
-export { readMarkdown } from "./input.ts";
-export { renderMarkdown } from "./render.ts";
-export type { RenderOptions } from "./render.ts";
-
 if (import.meta.main) {
   process.stdout.on("error", (error: NodeJS.ErrnoException) => {
     if (error.code === "EPIPE") {
